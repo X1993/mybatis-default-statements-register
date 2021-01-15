688d904b284319cc273a424c7717328249d39cbc
jie <275899116@qq.com>
2021-01-15 15:17:24 +0800

    添加测试所需表schema.sql

--- spring-boot-starter-mdsr-sample/src/main/resources/db/schema.sql
+++ spring-boot-starter-mdsr-sample/src/main/resources/db/schema.sql
@@ -10,3 +10,18 @@ CREATE TABLE user (
   version int(255) DEFAULT NULL COMMENT '版本号',
   removed bit(1) DEFAULT 0 COMMENT '是否已删除，1：已删除'
 ) DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `entity6`;
+CREATE TABLE `entity6` (
+  `id` varchar(50) ,
+  `id2` varchar(50) ,
+  `or` varchar(30) DEFAULT NULL,
+  `and` varchar(30) DEFAULT NULL,
+  `like` varchar(30) DEFAULT NULL,
+  `by` varchar(30) DEFAULT NULL,
+  `by_and_like` varchar(30) DEFAULT NULL,
+  `index` varchar(30) DEFAULT NULL,
+  `lo_code` varchar(30) DEFAULT NULL,
+  `removed` char(1) ,
+  CONSTRAINT table_entity6_pk PRIMARY KEY (id, id2)
+) DEFAULT CHARSET=utf8;
